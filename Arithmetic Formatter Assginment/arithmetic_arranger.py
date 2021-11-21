a0c0c8b7a023c7e830713a172d6afb461d0a86b2
r macrobbie <ryanmacrobbie@hotmail.com>
2021-11-20 20:11:37 -0600

    Second attempt. Actually adding numbers this time

--- Arithmetic Formatter Assginment/arithmetic_arranger.py
+++ Arithmetic Formatter Assginment/arithmetic_arranger.py
@@ -9,12 +9,13 @@ def arithmetic_arranger(problems,show_answers = False):
     #appending beginning spaces to the top line
     top += "  "
     for index,problem in enumerate(problems):
+      print(index)
       if index > 0:
-        top = "    "
-        bottom = "    "
-        line = "    "
+        top += "    "
+        bottom += "    "
+        line += "    "
         if show_answers:
-          solution = "    "
+          solution += "    "
 
 
       split_problem = problem.split()
@@ -23,14 +24,16 @@ def arithmetic_arranger(problems,show_answers = False):
       
       if not(split_problem[0].isdigit() or split_problem[2].isdigit()):
         arranged_problems = "Error: Numbers must only contain digits."
+        print("1!")
         break
       elif (len(split_problem[0]) or len(split_problem[2])) > 4:
         arranged_problems = "Error: Numbers cannot be more than four digits."
+        print("2!")
         break
-      elif not(split_problem[0] == "-" or split_problem[0] == "+"):
+      elif split_problem[1] != "-" and split_problem[1] != "+":
         arranged_problems = "Error: Operator must be '+' or '-'."
+        print("3!")
         break
-
       #appending starting section to their appropriate string
       bottom += split_problem[1] + " "
       if show_answers:
@@ -38,11 +41,17 @@ def arithmetic_arranger(problems,show_answers = False):
 
       if (len(split_problem[0]) > len(split_problem[2])):
         bottom += (" " * abs(len(split_problem[0])-len(split_problem[2])))  
-        line += '-' * len(split_problem[0])
+        line += '-' * (len(split_problem[0]) + 2)
       else:
         top += (" " * abs(len(split_problem[0])-len(split_problem[2])))
-        line += '-' * len(split_problem[2])  
-      
+        line += '-' * (len(split_problem[2]) + 2)
+
+      top += split_problem[0]
+      bottom += split_problem[2]
+      print(top)  
+      print(bottom)
+      print(line)
+
     top = "\n"
     bottom = "\n"
     line = "\n"
@@ -50,5 +59,7 @@ def arithmetic_arranger(problems,show_answers = False):
     arranged_problems = top + bottom + line
     if show_answers:
       arranged_problems += solution
+
+    print(arranged_problems)
 
   return arranged_problems
