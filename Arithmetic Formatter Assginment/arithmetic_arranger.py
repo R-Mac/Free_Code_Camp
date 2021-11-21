eecf54e2180f3a0a4ba0ebce5d20ce7c8638938e
r macrobbie <ryanmacrobbie@hotmail.com>
2021-11-20 20:39:27 -0600

    cleaning cod

--- Arithmetic Formatter Assginment/arithmetic_arranger.py
+++ Arithmetic Formatter Assginment/arithmetic_arranger.py
@@ -11,7 +11,6 @@ def arithmetic_arranger(problems,show_answers = False):
     #appending beginning spaces to the top line
     top += "  "
     for index,problem in enumerate(problems):
-      #print(index)
       if index > 0:
         top += "      "
         bottom += "    "
@@ -27,17 +26,14 @@ def arithmetic_arranger(problems,show_answers = False):
       if not(split_problem[0].isdigit()) or not(split_problem[2].isdigit()):
         arranged_problems = "Error: Numbers must only contain digits."
         error_status = True
-        print("3!")
         break
       elif (len(split_problem[0]) > 4) or (len(split_problem[2]) > 4):
         arranged_problems = "Error: Numbers cannot be more than four digits."
         error_status = True
-        print("2!")
         break
       elif split_problem[1] != "-" and split_problem[1] != "+":
         arranged_problems = "Error: Operator must be '+' or '-'."
         error_status = True
-        print("4!")
         break
       #appending starting section to their appropriate string
       bottom += split_problem[1] + " "
@@ -65,5 +61,4 @@ def arithmetic_arranger(problems,show_answers = False):
         arranged_problems += "\n"
         arranged_problems += solution
 
-  print(arranged_problems)
   return arranged_problems
